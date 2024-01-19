edc2bc5fa04e603a23df1eeea23c6127eb8e82e8
Lohana Orellano <lohanaorellano_lsi@hotmail.comgit>
2024-01-19 13:07:44 +0100

    correccion para deploy

--- components/Producto.js
+++ components/Producto.js
@@ -11,7 +11,7 @@ export default function Producto({producto}) {
         alt={producto.nombre}
         />
         <div className="p-5">
-            <h3 className="text-2xl font-bold">{producto.nombre}"</h3>
+            <h3 className="text-2xl font-bold">{producto.nombre}</h3>
             <p className="mt-5 font-bold text-4xl text-amber-500">{formatearDinero(producto.precio)}</p>
             
             <button 
